68366c5ed7285f0bd1644924958a20f2f12d4f09
Qi Zhu <115549630@qq.com>
2024-07-22 07:36:56 +0000

    G: update solutions

--- problems/G/check.sh
+++ problems/G/check.sh
@@ -3,16 +3,17 @@ set -e
 cd data
 bash generator.sh
 cp ../../../testlib.h testlib.h
+echo "Compiling Checker"
 g++ checker.cpp -o checker.tmp -std=c++14 -O2
 cd ..
 
 bash judge.sh code/bf.cpp 0 5
 bash judge.sh code/bf_bitset.cpp 0 6
 bash judge.sh code/dfs.cpp 1 8
-bash judge.sh code/bruckets.cpp 2 9
-bash judge.sh code/meet_in_the_middle.cpp 2 10
-bash judge.sh code/two_parts_random.cpp 4 11
+bash judge.sh code/bruckets.cpp 2 8
 bash judge.sh code/mod/mod_one_prime.cpp 1 9
-bash judge.sh code/mod/mod_two_primes.cpp 2 10
-bash judge.sh code/mod/mod_1e6.cpp 3 10
+bash judge.sh code/mod/mod_two_primes.cpp 1 9
+bash judge.sh code/two_parts_random.cpp 1 10
+bash judge.sh code/meet_in_the_middle.cpp 2 10
+bash judge.sh code/mod/mod_1e6.cpp 2 10
 bash judge.sh code/mod/mod_bitset.cpp 4 11
