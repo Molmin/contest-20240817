990ec17172d7be57239664b4116e4ef67a8d112d
Milmon <milmon@qq.com>
2024-08-17 12:10:37 +0800

    enable check

--- check.sh
+++ check.sh
@@ -5,37 +5,37 @@ cd problems/A
 bash check.sh
 cd ../..
 
-# echo "==================== Problem B ===================="
-# cd problems/B
-# bash check.sh
-# cd ../..
+echo "==================== Problem B ===================="
+cd problems/B
+bash check.sh
+cd ../..
 
-# echo "==================== Problem C ===================="
-# cd problems/C
-# bash check.sh
-# cd ../..
+echo "==================== Problem C ===================="
+cd problems/C
+bash check.sh
+cd ../..
 
-# echo "==================== Problem D ===================="
-# cd problems/D
-# bash check.sh
-# cd ../..
+echo "==================== Problem D ===================="
+cd problems/D
+bash check.sh
+cd ../..
 
-# echo "==================== Problem E ===================="
-# cd problems/E
-# bash check.sh
-# cd ../..
+echo "==================== Problem E ===================="
+cd problems/E
+bash check.sh
+cd ../..
 
-# echo "==================== Problem F ===================="
-# cd problems/F
-# bash check.sh
-# cd ../..
+echo "==================== Problem F ===================="
+cd problems/F
+bash check.sh
+cd ../..
 
-# echo "==================== Problem G ===================="
-# cd problems/G
-# bash check.sh
-# cd ../..
+echo "==================== Problem G ===================="
+cd problems/G
+bash check.sh
+cd ../..
 
-# echo "==================== Problem H ===================="
-# cd problems/H
-# bash check.sh
-# cd ../..
+echo "==================== Problem H ===================="
+cd problems/H
+bash check.sh
+cd ../..
